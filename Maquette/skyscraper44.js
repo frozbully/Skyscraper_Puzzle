8cd044ea680a8af7d3901eb47fcb0de10f94f45e
frozbully <f.rozbully@gmail.com>
2019-10-01 17:31:04 +0200

    fix

--- Maquette/skyscraper44.js
+++ Maquette/skyscraper44.js
@@ -1,7 +1,44 @@
-var gameTab = document.querySelectorAll("th");
+var gameTab;
+var cel;
+var activeCel;
+var i;
 
-for(var i=0;i<36;i++){
-  gameTab[i].addEvenListener("click", function() {
-    gameTab[i].textContent = 1;
-    });
+function Init() {
+    gameTab = document.querySelectorAll("th");
+    initlisteners();
 }
+
+function initlisteners() {
+  for (i=0;i<6*6;i++) {
+    cel = gameTab[i];
+    if(cel.id == ''){
+      cel.addEventListener("click",function(){makeActive(this);});
+    }
+  }
+  window.addEventListener("keydown",function(event){writeActive(event.keyCode);},true);
+}
+
+function makeActive(cel){
+  if (activeCel !== undefined) {
+    activeCel.id = '';
+  }
+  activeCel = cel;
+  activeCel.id = "active";
+}
+
+function writeActive(n){
+  if (activeCel !== undefined) {
+    if (n>96 && n<=100) {
+    activeCel.textContent = n-96;
+    }
+    if (n === 96){
+      activeCel.textContent = '';
+    }
+  }
+}
+
+function main() {
+  Init();
+}
+
+main();
